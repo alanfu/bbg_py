a5852570056de4f3a8df8c7f45bf5920d0cc6f34
alanfu <alanfyr@gmail.com>
2016-07-14 00:24:46 -0400

    Support DataFrame return

--- Bloomberg_Wrapper.py
+++ Bloomberg_Wrapper.py
@@ -7,6 +7,9 @@
 import blpapi
 from optparse import optionParser
 import datetime as dt
+from collections import OrderedDict
+import numpy as np
+import pandas as pd
 
 BBG_REF_DATA_SERVICE = '//blp/refdata'
 BBG_REF_DATA_REQUEST = 'ReferenceDataRequest'
@@ -97,11 +100,11 @@ class BBGDataMgr():
 		except Exception:
 			raise
 
-	def bdh(self, sec, field, date):
+	def bdh(self, sec, field, *date):
 		###################################
 		### @sec: str
 		### @field: str
-		### @date: datetime.datetime
+		### @date: tuple(datetime.datetime)
 		###################################
 
 		# append securities to request
@@ -118,13 +121,24 @@ class BBGDataMgr():
 		else:
 			self._ref.getElement(BBG_REQUEST_FIELD).appendValue(field)
 
-		self._req.set(BBG_REQUEST_START_DT, date.strftime(DATE_FORMAT))
-		self._req.set(BBG_REQUEST_END_DT, date.strftime(DATE_FORMAT))
+		if len(date) == 1:
+			start_dt = date[0]
+		elif len(date) == 2:
+			start_dt = date[0]
+			end_dt = date[1]
+		else:
+			start_dt = dt.datetime.today()
+			end_dt = start_dt
+			if len(date) > 2:
+				print('Too many date arguments!')
+		
+		self._req.set(BBG_REQUEST_START_DT, start_dt.strftime(DATE_FORMAT))
+		self._req.set(BBG_REQUEST_END_DT, end_dt.strftime(DATE_FORMAT))
 
 		self._session.sendRequest(self._ref)
 
 		loop = True
-
+		errorMsg = str()
 		try:
 			while loop:
 				event = self._session.nextEvent()
@@ -133,14 +147,48 @@ class BBGDataMgr():
 					if event.eventType() = blpapi.Event.RESPONSE or \
 					event.eventType() = blpapi.Event.PARTIAL_RESPONSE:
 						securityData = msg.getElement(BBG_SECURITY_DATA)
+						
+						# check for invalid security
+						if securityData.hasElement('securityError'):
+							securityError = securityData.getElement('securityError')
+							errorMsg = securityError.getElementAsString('message')
+							raise ValueError(errorMsg)
+						# check for invalid field
+						else:
+							fieldExceptions = securityData.getElement('fieldExceptions')
+							if fieldExceptions.numValues() > 0:
+								missingField = fieldExceptions.getValueAsElement(0)
+								field_id = missingField.getElementAsString('fieldId')
+								error_info = missingField.getElement('errorInfo')
+								error_msg = error_info.getElementAsString('message')
+								error_msg = error_msg + ": " + field_id
+								raise ValueError(errorMsg)
+						
+						name = securityData.getElementAsString('security')		
 						fieldData = securityData.getElement(BBG_FIELD_DATA)
-						element = fieldData.getValueAsElement(0)
-						ret = element.getElement(1)
-
-						return ret.getValue()
-
+						response = OrderedDict()
+						
+						for i in range(fieldData.numValues()):
+							element = fieldData.getValueAsElement(i)
+							for n in range(1, element.numElements()):
+								date = element.getElementAsDatetime('date')
+								field = element.getElement(n)
+								response[date] = field.getValue()
+						
+						ret = pd.DateFrame.from_dict(response, orient = 'index')
+						
+						if len(ret) == 1:
+							return ret.ix[0][0]
+						elif len(ret) > 1:
+							return ret
+						else:
+							raise ValueError('No data found!\n')
+							
 					if event.eventType() == blpapi.Event.RESPONSE:
 						loop = False
+		except ValueError as e:
+			print(e，'\nsecurity: ',sec,'\nfield: ',field,'\nperiod: ',start_dt,'-',end_dt)
+			return float('nan')
 		except Exception:
 			raise
 
@@ -152,3 +200,4 @@ if __name__ == '__main__':
 
 	# Expirty date of June SPX call 2000
 	ret3 = bbg.bdp('X5M6C 2000 Index', 'LAST_TRADEABLE_DT')
+	ret4 = bbg.bdh('SPY US 12/21/13 C178 Equity', 'IVOL_MID', dt.datetime(2013,11,13))
